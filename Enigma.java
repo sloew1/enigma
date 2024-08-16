3f47981954da3b928115ce34ad5675c4e15e23ed
Pasxal-rgb <moralverkehr89@gmail.com>
2024-08-16 17:07:31 +0200

    Experimente mit dem Konstruktor der Enigma

---
+++ Enigma.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class Enigma {
+    private Rotor rotor3;
+    private Rotor rotor2;
+    private Rotor rotor1;
+    int anzahlRotoren = 0;
+    Plugboard p;
+    Rotor r;
+    HashMap<Character, Character> connections = new HashMap<>();
+
+    private static Enigma e;
+
+
+    private Enigma(int anzahlRotoren, Plugboard p, char letter, char letter2){
+        this.anzahlRotoren =anzahlRotoren;
+        try {
+            this.p = new Plugboard(connections);
+            p.setConnections(letter, letter2);
+            if (anzahlRotoren == 1) this.rotor1 = new Rotor();
+            if (anzahlRotoren == 2) {
+                this.rotor1 = new Rotor();
+                this.rotor2 = new Rotor();
+            }
+            if (anzahlRotoren == 3) {
+                this.rotor1 = new Rotor();
+                this.rotor2 = new Rotor();
+                this.rotor3 = new Rotor();
+            }
+        }catch (Exception e){
+            throw new IllegalArgumentException("Falscher EIngabewert angegeben");
+        }
+    }
+
+    public static Enigma createEnigma(int anzahlRotoren, Plugboard p, Rotor r){
+        if(e == null) {
+            e = new Enigma(anzahlRotoren, p, r);
+        }
+        return e;
+    }
+}
