4f28cff941b23c9234479e7a208f0ce8b8edfe49
sloew2 <kenshin88@web.de>
2024-08-28 10:48:41 +0200

    console

--- Main.java
+++ Main.java
@@ -1,68 +1,84 @@
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Enigma eni = Enigma.createEnigma(2);
+        System.out.println("Enigma startet...");
+        System.out.println("Modus: ENCRYPTION");
+
+        enum Mode {
+            ENCRYPT,
+            DECRYPT
+        }
+        Mode currentMode = Mode.ENCRYPT; // Initial mode is ENCRYPT
+
+        Scanner scanner = new Scanner(System.in);
+        String consoleOutput = ""; // Das komplette Wort, das in der Konsole angezeigt wird. Erweitert sich immer um einen Buchstaben bei EIngabe
         
+        // Enigma erzeugen und startMappings der Rotoren speichern
+        Enigma eni = Enigma.createEnigma(2);
         ArrayList<HashMap<Character, Character>> startMappings = new ArrayList<>();
         startMappings = eni.getStartMappings();              
+       
+        // Eingaben über die Konsole auswerten:
+        while (true) {            
+            String input = scanner.nextLine();
 
-        try {
-            int ch;
-            
-            while ((ch = System.in.read()) != -1) {
-                char character = (char) ch;
-                if (character != '\n' && character != '\r') {                    
-                    processCharacter(Character.toUpperCase(character), eni);
+            // Umschalten zwischen Ver- und Entschlüsselung:
+            if (input.equals(":d")) {
+                currentMode = Mode.DECRYPT;
+                consoleOutput = "";
+                
+                eni.setStartMappings(startMappings); // Rotoren auf Start-Einstellung zurücksetzen
+
+                System.out.println("Wechsel zum DECRYPTION-Modus.");
+                continue;
+            } else if (input.equals(":e")) {
+                currentMode = Mode.ENCRYPT;
+                consoleOutput = "";
+                System.out.println("Wechsel zum ENCRYPTION-Modus.");
+                continue;
+            }
+
+            // Konsoleneingabe (String) in Character umwandeln:
+            char character;
+            if (input.length() == 1) {
+                // Extrahiere das erste (und einzige) Zeichen als char
+                character = input.charAt(0);    
+                character = Character.toUpperCase(character);            
+            } else {
+                System.out.println("Eingabe darf nicht länger, als ein Zeichen sein");
+                continue;
+            }
+
+            // Je nach Modus den Character ver- oder entschlüsseln:
+            Character c = ' ';
+            switch (currentMode) {
+                case ENCRYPT -> {
+                    c = encrypt(character, eni);                    
+                    consoleOutput += c;
+                    System.out.println("Ausgabe (verschlüsselt): "+consoleOutput);
                 }
-
-                // Beenden, wenn der Benutzer 'q' eingibt
-                if (character == 'q') {
-                    break;
+                case DECRYPT -> {
+                    c = decrypt(character, eni);                    
+                    consoleOutput += c;
+                    System.out.println("Ausgabe (entschlüsselt): "+consoleOutput);
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        
+            
+        }        
     }
 
-    public static void processCharacter(char character, Enigma eni) {
-        // Hier die gewünschte Verarbeitung des Zeichens
-        System.out.println("Verarbeitetes Zeichen: " + character);
-
-        // String input = "HELLOWORLDS";
-        String output = "";
-                
-        Character c = eni.encryptCharacter(character);
-        output += c;
-        
-        System.out.println("Encrypted: " + output);
-
-        /*
-        // Rotoren auf Anfang zurückstellen:
-        eni.setStartMappings(startMappings);
-
-        // Entschlüsseln:
-        System.out.println("");
-        System.out.println("Entschlüsseln:");
-
-        String input2 = output;
-        String output2 = "";
-        
-
-        for (char character : input2.toCharArray()) {
-            System.out.println("");
-            Character c = eni.decryptCharacter(character);
-            output2 += c;
-        }
-
-        System.out.println("Decrypted: " + output2);
-        */
-    }            
+    public static Character encrypt(char character, Enigma eni) {                
+        Character c = eni.encryptCharacter(character);            
+        return c;
+    } 
+    
+    public static Character decrypt(char character, Enigma eni) {
+        Character c = eni.decryptCharacter(character);
+        return c;
+    }
 }
